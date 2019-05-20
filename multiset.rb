cc276382a562521900c9e9a5f0bf657bc738b5e2
Leta <leta@berkeley.edu>
2019-05-20 16:55:56 -0700

    make self method call a parent method to init

--- multiset.rb
+++ multiset.rb
@@ -2,13 +2,14 @@
 
 # https://github.com/maraigue/multiset/blob/master/lib/multiset.rb
 # https://www.rubyguides.com/2018/06/rubys-method-arguments/
+# https://medium.com/@ethan.reid.roberts/rubys-anonymous-eigenclass-putting-the-ei-in-team-ebc1e8f8d668
 
-## class MSet. initialized with a hash
-## hash to store members (keys) and their counts (values)
-## ArgumentError if val is less than 1 or not to_i
-## returns count for a given member OR nil if member not exists
-## be initialized with _any_ Enumerable, in which case it traverses the Enumerable, adding 1 for every member it finds.
-## besides Mset.new, you can construct a multiset with the [] Mset class method. ; m = Mset[ham:13, ram:22]
+# class MSet. initialized with a hash
+# hash to store members (keys) and their counts (values)
+# ArgumentError if val is less than 1 or not to_i
+# returns count for a given member OR nil if member not exists
+# be initialized with _any_ Enumerable, in which case it traverses the Enumerable, adding 1 for every member it finds.
+# besides Mset.new, you can construct a multiset with the [] Mset class method. ; m = Mset[ham:13, ram:22]
 # add two multisets
 # to_h : return a new hash with the same content as the MSet
 # to_a : create element for each element of Mset times it's count
@@ -28,22 +29,39 @@ puts "hello"
 
 class Mset
 	include Enumerable
-
 	attr_accessor :a_hash
 
-	def initialize(*list, **args)
+
+	def hash_to_hash(brown) 
+		brown.each {|k, v| 
+			validate!(v)
+			a_hash[k] = v #overwrites duplicate keys, expected hash behavior 
+		}
+	end
+
+
+	def initialize(args)
 		@a_hash = Hash.new
-puts args
-		if (args.count > 0) 
-			args.each {|k, v| 
-				validate!(v)
-				a_hash[k] = v #overwrites duplicate keys, expected hash behavior 
-			}
-		end
-	end #init
+
+		if (defined?(args))
+			puts "args is #{args}"
+			puts args.class
+			if(args.class == Hash)
+				hash_to_hash(args)
+			end
+
+			if(args.class == Array)
+					puts args.count
+					if(args.count == 1)
+						puts "it is one"
+					end
+			end
+
+		end #if defined
+	end #initialize
 
 
-	def each 
+	def each
 		yield a_hash
 	end
 
@@ -59,62 +77,43 @@ puts args
 	end #validate
 
 
-	def self.[](*list)	
-		puts 'in self'
-    initialize(list)
+	def self.[](args)
+		puts "in self"
+		puts args.class
+		puts args.count
+		puts "a hash is"
+		puts @a_hash
+		if (defined?(args) && args.count > 0)
+			puts "args is #{args}"
+			self.hash_to_hash(args)
+		end
 	end
 
 
- 	def [](*args) #can use key: as an arg
- 	  puts 'in brackets'	
- 	  puts args.class
- 		if (args.class == Hash) #a hash
- 			puts 'a hash'
- 			initialize(args)
- 		elsif (args.count == 1 && args.class != Array || args.class == Key)	#a key
- 			count_for_key(args[0])
- 		else #a set 
- 			puts 'a set'
- 			#puts args
-			args.each {|k, v|
-				k.class
-				v.class
-				puts "#{k} and #{v}"
-				if(a_hash.has_key?(k))
-					a_hash[k] += 1
-				else 
-					a_hash[k] = 1
-				end
-			}
-			a_hash
- 		end
- 	end 		
+	def [](*list)
+		puts "in brackets"
+		puts list.class
+		puts list.count
+		if(list.count == 1 && list.class == Array)
+			count_for_key(list[0])
+		end
+
+		#initialize(list)
+	end
 
 
 	def count_for_key(key)
+		puts "count for key"
 		if(a_hash.has_key?(key.to_sym))
 			a_hash[key.to_sym]
 		else nil
 		end
 	end #count for key
 
-
-	def add_two(a, b)
-		puts a.class
-		puts b.class
-		if (a.class == Hash && b.class == Hash)
-			puts 'two hashes'
-			a.each {|k, v|
-				b[k] += v
-			}
-			puts b
-		end
-	end #add two
-
 end #class Mset
 
 
-=begin
+
 h = {a:23, b:2, c: 9}
 m = Mset.new(h)
 puts m[:a]
@@ -123,11 +122,13 @@ puts m.count_for_key('3')
 puts m.count_for_key('b')
 n = Mset.new(foo:12, bar:18)
 puts n[:bar] 
-=end
-#n = Mset[ham:13, ram:22]
-a = Mset.new[1,3,3,3,4,5,5]
-b = Mset.new({3 => 12, 4 => 1})
-puts b
+
+q = Mset[ham:13, ram:22]
+#q = Mset.new[ham:13, ram:22]
+puts q
+#a = [1,3,3,3,4,5,5]
+#b = Mset.new({3 => 12, 4 => 1})
+#puts b
 #c.add_two(a, b)
 #puts c
 #puts c
